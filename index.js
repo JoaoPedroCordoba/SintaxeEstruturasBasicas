8f3553e9251e9adc2c544d96b6781cf57cbf15f0
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-10-27 21:58:26 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,117 @@
+let mensagem = 'hello word';
+const mensagem2 = 'hello word2';
+console.log(mensagem);
+console.log(mensagem2);
+
+
+//tipos de dado
+//number
+let a =5;
+let b = 15.67;
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(b / a);
+
+
+console.log(a + 'etec');
+console.log(a - 'etec');
+console.log(a / 'etec');
+console.log(a * 'etec');
+
+
+//string
+let nome  = 'João Pedro'
+let sobrenome= "cordoba Rodrigues"
+let nomeCompleto= `Meu nome Completo é ${nome} ${sobrenome}`
+console.log({nomeCompleto});
+
+//boolean
+let formValido = true
+let fromIvalido = false
+
+//undefined e null
+
+
+
+//objet
+let aluno1 = {};
+let aluno = {nome: "zé", idade: 30};
+console.log(aluno)
+
+
+//conversções
+console.log('o resultado é' + 17.65);
+console.log('o resultado é' - 17.65);
+String (17.65);
+//retorna uam string
+
+
+Number(17.65);
+Number (true);
+// retorna 1
+Number (false);
+// retorna  0
+Number (null);
+// retorna 0
+Number ('');
+//retorna 0
+Number ('1 hdshdhdhd')
+//retorna Nan
+Number ('1    150')
+//retorna Nan
+
+Boolean('')
+//false
+Boolean(0)
+//false
+Boolean(null)
+//false
+Boolean(undefined)
+//false
+Boolean(Nan)
+//false
+
+Boolean(' ')
+//true
+
+//operadores
+let c =5;
+let d = 15.67;
+console.log(c + d);
+//soma
+console.log(c - d);
+// subtração
+console.log(c / d);
+//divisão
+console.log(c * d);
+//multiplicação
+console.log(c / d);
+//divisão
+console.log(c % d);
+//Resto da divisão inteira
+console.log(c ** d);
+//Exponenciação
+
+//operadores logicos
+//  || operador 'ou';
+//  && operador 'e';
+//  ! operador 'not'.
+//  ?? retorna b se a for null ou undefined, senão retorna a.
+
+//Conversões
+//  > < menor ou maior 
+//  <= >= maior ou igual, menor ou igual
+//  == igualdade
+//  != desigualdade 
+//  != igualdade restrita 
+//  !== Desigualdade restrita
+
+
+
+
+// na mesma linha
+// inicio da linha
+/*bloco*/
+
